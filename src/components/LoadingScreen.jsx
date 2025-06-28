4f8696d438602538016236f9d94ba22b65c77808
Andrea Perna <and.perna99@gmail.com>
2025-06-28 11:38:30 +0200

    Update portfolio site

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<Keep The Gradient/>";
+  const fullText = "<Be water, my friend/>";
 
   useEffect(() => {
     let index = 0;
